9d3ea32430eea8814ceb32c7be8bddd9b3d08848
DeepThoughtt <dvitulli@libero.it>
2026-04-16 01:18:09 +0200

    Business logic refactoring

--- src/business/app_business.py
+++ src/business/app_business.py
@@ -1,18 +1,21 @@
 import os
 import shutil
+import pathlib
 
 class AppBusiness:
 
     @staticmethod
     def convert(directory, from_ext, to_ext, flags):
+        directory = pathlib.Path(directory)
         converted_elements = 0
 
         if not flags.recursive_mode:
-            filenames = set(next(os.walk(directory, topdown = True))[2])
+            filenames = {p.name for p in directory.iterdir() if p.is_file()}
             converted_elements, stopped = AppBusiness.convert_directory_content(directory, filenames, from_ext, to_ext, flags)
             return converted_elements, stopped
         
         for path, directories, filenames in os.walk(directory):
+            path = pathlib.Path(path)
             filenames = set(filenames)
             new_converted_elements, stopped = AppBusiness.convert_directory_content(path, filenames, from_ext, to_ext, flags)
             converted_elements += new_converted_elements
@@ -24,36 +27,36 @@ class AppBusiness:
     
     @staticmethod
     def convert_directory_content(directory, filenames, from_ext, to_ext, flags):
-        backup_directory = f"{directory}/{from_ext}_backup_folder"
+        backup_directory = directory / f"{from_ext}_backup_folder"
         converted_elements = 0
         
         for element in filenames:
             if not flags.operation_in_progress:
                 return converted_elements, True
-                
-            original_file = f"{directory}/{element}"
             
-            if element.lower().endswith(f".{from_ext}"):
-                if flags.create_backups:
-                    if not os.path.exists(backup_directory):
-                        os.mkdir(backup_directory)
+            if not element.lower().endswith(f".{from_ext}"):
+                continue
                 
-                    shutil.copyfile(original_file, f"{backup_directory}/{element}")
-                
-                filename = os.path.splitext(element)[0]
-                new_name = AppBusiness.create_new_file_name(directory, filename, to_ext, filenames)
-                os.rename(original_file, new_name)
-                converted_elements += 1
+            original_file = directory / element
+
+            if flags.create_backups:
+                backup_directory.mkdir(exist_ok = True)
+                shutil.copyfile(original_file, backup_directory / element)
+            
+            filename = pathlib.Path(element).stem
+            new_name = AppBusiness.create_new_file_name(directory, filename, to_ext, filenames)
+            original_file.rename(new_name)
+            converted_elements += 1
 
         return converted_elements, False
     
     @staticmethod
     def create_new_file_name(directory, filename, to_ext, directory_content):
         if f"{filename}.{to_ext}" not in directory_content:
-            return f"{directory}/{filename}.{to_ext}"
+            return directory / f"{filename}.{to_ext}"
         
         n = 1
         while f"{filename}_{n}.{to_ext}" in directory_content:
             n += 1
         
-        return f"{directory}/{filename}_{n}.{to_ext}"
+        return directory / f"{filename}_{n}.{to_ext}"
