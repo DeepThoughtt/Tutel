f60c0661a4ce01cc042eb176e8faaf13b897ada6
DeepThoughtt <dvitulli@libero.it>
2026-05-04 01:37:13 +0200

    Update icons.py

--- src/consts/icons.py
+++ src/consts/icons.py
@@ -1,2 +1,12 @@
 class Icons:
     TUTEL = "tutel.ico"
+
+    @staticmethod
+    def to_set():
+        return {
+            Icons.TUTEL,
+        }
+    
+    @staticmethod
+    def is_valid(value):
+        return value in Icons.to_set()
